3b6bf8e7666905e929c9e26ed0341cc97646004e
Mohit Kumar <mohitahlawat912@gmail.com>
2025-12-31 12:03:36 +0530

    Lecture_41

---
+++ Lecture_41/3_React_Installation/src/App.jsx
@@ -0,0 +1,9 @@
+
+import "./index.css"
+export function Heading(){ // component
+  return (
+    <h2>this is h2</h2>
+  )
+}
+
+
---
+++ Lecture_41/3_React_Installation/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {Heading} from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <Heading />
+)
